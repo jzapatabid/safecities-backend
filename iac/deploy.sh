71c3722c7bb2885f4093126d19fa046fa460b05a
Nicolas Viviescas <nicolas.viviescas@sinapsis.global>
2024-04-19 13:10:06 -0500

    update repo

--- iac/deploy.sh
+++
@@ -1,6 +0,0 @@
-az login
-
-az deployment group validate --resource-group rg-d-safecities --subscription "Sinapsis Azure Dev/Test" --template-file vmtemplate.json --parameters vmtemplate.parameters.json
-
-az deployment group create --mode Incremental --name safecities001 --resource-group rg-d-safecities --subscription "Sinapsis Azure Dev/Test" --template-file vmtemplate.json --parameters vmtemplate.parameters.json
-
